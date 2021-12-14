b388d9e2adc62fb96ef2b241e5842499f8ede255
Glenn Dixon Jr <glenndixon19@gmail.com>
2021-12-13 21:00:58 -0600

    imported serviceform and invoked the function

--- src/scripts/SinkRepair.js
+++ src/scripts/SinkRepair.js
@@ -1,9 +1,11 @@
+import { ServiceForm } from "./ServiceForm.js"
 import { Requests } from "./Request.js"
 
 export const SinkRepair = () => {
     return `
     <h1>Maude and Merle's Sink Repair</h1>
     <section class="serviceForm">
+        ${ServiceForm()}
     </section>
 
     <section class="serviceRequests">
