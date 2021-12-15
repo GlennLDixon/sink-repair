f4467d20ae1f152180b9b63e606158c68c65085d
Glenn Dixon Jr <glenndixon19@gmail.com>
2021-12-14 21:04:36 -0600

    stored the container in mainContainer const and dispatced a new customevent to main conainter and also created a deleteRequest function to mainContainer

--- src/scripts/dataAccess.js
+++ src/scripts/dataAccess.js
@@ -2,6 +2,8 @@ const applicationState = {
     requests: []
 }
 
+const mainContainer = document.querySelector("#container")
+
 const API = "http://localhost:8088"
 
 export const fetchRequests = () => {
@@ -32,7 +34,16 @@ export const sendRequest = (userServiceRequest) => {
     return fetch(`${API}/requests`, fetchOptions)
         .then(response => response.json())
         .then(() => {
-
+            mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
         })
+}
+
+export const deleteRequest = (id) => {
+    return fetch(`${API}/requests/${id}`, { method: "DELETE" })
+        .then(
+            () => {
+                mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+            }
+        )
 }
 
