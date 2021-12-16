3dbfab7a5ea33410a9cf9cc84864009b8d4a161e
Glenn Dixon Jr <glenndixon19@gmail.com>
2021-12-15 21:18:25 -0600

    added the select tag in the convertRequestToListElement

--- src/scripts/Request.js
+++ src/scripts/Request.js
@@ -10,15 +10,52 @@ mainContainer.addEventListener("click", click => {
     }
 })
 
+mainContainer.addEventListener(
+    "change",
+    (event) => {
+        if (event.target.id === "plumbers") {
+            const [requestId, plumberId] = event.target.value.split("--")
+
+            /*
+                This object should have 3 properties
+                   1. requestId
+                   2. plumberId
+                   3. date_created
+            */
+            const completion = { }
+
+            /*
+                Invoke the function that performs the POST request
+                to the `completions` resource for your API. Send the
+                completion object as a parameter.
+             */
+
+        }
+    }
+)
+
 
 const convertRequestToListElement = (request) => {
-    return `
+    let html = `
         <li>${request.description}
         <button class="request_delete"
             id="request--${request.id}">
             Delete
         </button>
     </li>`
+
+    html += `<select class="plumbers" id="plumbers">
+    <option value="">Choose</option>
+    ${
+        plumbers.map(
+            plumber => {
+                return `<option value="${request.id}--${plumber.id}">${plumber.name}</option>`
+            }
+        ).join("")
+    }
+    </select>`
+
+    return html
 }
 
 export const Requests = () => {
